488e7f31ba8f4126a9cd3448c6bc089635245425
kada <soulayman.kada@gmail.com>
2021-07-10 01:17:21 +0100

    Update addd.php

--- addd.php
+++ addd.php
@@ -1,22 +1,24 @@
 <?php
+echo "Test is safe";
 
-$host="sql4.freesqldatabase.com";
-$user="sql4423835";
-$password="1V4JTSVxl4";
-$database="sql4423835";
-$port="3306";
 
-$connect = new mysqli($host, $user, $password, $database, $port);
+// $host="sql4.freesqldatabase.com";
+// $user="sql4423835";
+// $password="1V4JTSVxl4";
+// $database="sql4423835";
+// $port="3306";
+
+// $connect = new mysqli($host, $user, $password, $database, $port);
 
 // Get Vars
-$FirstName = $_POST["firstname"];
-$LastName = $_POST["lastname"];
-$Email = $_POST["email"];
-$Password = $_POST["password"];
+// $FirstName = $_POST["firstname"];
+// $LastName = $_POST["lastname"];
+// $Email = $_POST["email"];
+// $Password = $_POST["password"];
 
-$stmt = $connect->prepare("insert into USERS (Email, Password, FirstName, LastName) values (?,?,?,?)");
-$stmt->bind_param("ssss", $Email, $Password, $FirstName, $LastName);
-$stmt->execute();
-echo "Done";
+// $stmt = $connect->prepare("insert into USERS (Email, Password, FirstName, LastName) values (?,?,?,?)");
+// $stmt->bind_param("ssss", $Email, $Password, $FirstName, $LastName);
+// $stmt->execute();
+// echo "Done";
 
 ?>
